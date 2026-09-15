5af651393ae522830399740e3b453100566a6c8e
Renato Ganske Junior <renatoganske.jr@gmail.com>
2026-09-14 21:23:23 -0300

    Calcula e imprime números divisíveis por 3.

---
+++ src/CursoLogicaCapitulo06/Exercicio01.java
@@ -0,0 +1,22 @@
+package CursoLogicaCapitulo06;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        Integer[] numeros = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        for (int i = 0; i < numeros.length; i++) {
+            Integer numAtual = numeros[i];
+            Integer moduloPorTres = numAtual % 3;
+
+
+            if (moduloPorTres.equals(0)) {
+
+                System.out.println("O número " + numAtual + " é divisível por 3.");
+            }
+
+        }
+
+        System.out.println("Fim do programa.");
+    }
+}
